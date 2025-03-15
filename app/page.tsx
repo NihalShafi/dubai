502bd4d0fb7c9337db2f6620a618744358ff3267
NihalShafi <nihalshafi09@gmail.com>
2025-03-15 10:42:24 +0530

    commited a fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import BookingForm from "./components/BookingForm";
 export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist)]">
-            <main>
+            <main className="p-10 flex flex-col mt-40">
                 <h1 className="text-4xl font-bold mb-6">Space Travel Booking</h1>
                 <BookingForm />
             </main>
